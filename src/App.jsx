215e89e1fb33ebc7fba0400a4a229fc9bc339147
AjayaBudha <ajaya.budhathoki@hotmail.com>
2023-05-15 23:52:52 +0100

    remove some comments

--- src/App.jsx
+++ src/App.jsx
@@ -17,27 +17,3 @@ export default function App() {
     </>
   );
 }
-
-function SongCard({ band }) {
-  return (
-    <div className="bandCard">
-      <h2 className="bandName">{band.name}</h2>
-      <img src={band.band_image}></img>
-      <p className="type">{band.type}</p>
-      <p className="description">{band.description}</p>
-      <div className="songs">
-        {band.songs.map((song) => {
-          <div key={song.name} className="songs">
-            <h3 classname="songName">{song.name}</h3>
-            <img src={song.cover_art}></img>
-            <div classname="release_date">{song.release_date}</div>
-          </div>;
-        })}
-      </div>
-    </div>
-  );
-
-  // function MyButton() {
-  //   return <button>I'm a button</button>;
-  // }
-}
